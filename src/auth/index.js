14544cc3c6d960c33753a887da4289785f6d647a
rohan <rohankhan170@gmail.com>
2019-09-13 23:02:58 +0600

    Changed return of Promise to be more descriptive

--- src/auth/index.js
+++ src/auth/index.js
@@ -2,9 +2,15 @@ export default (formData) => {
 	return new Promise((resolve, reject) => {
 		setTimeout(() => {
 			if (formData.username === 'Rohan' && formData.password === 'rohan123' ? true : false) {
-				return resolve({
+				resolve({
+					isLoggedIn: true,
 					username: formData.username,
 					loggedInAt: Date.now() / 1000
+				});
+			} else {
+				reject({
+					isLoggedIn: false,
+					loginFailedAt: Date.now() / 1000
 				});
 			}
 		}, 1000);
